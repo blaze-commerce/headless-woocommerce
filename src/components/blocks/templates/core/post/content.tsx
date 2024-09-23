6f17005d1fcab557dcbc5992f67f47c509b085f1
jv-lopez <93750956+jv-lopez@users.noreply.github.com>
2024-09-23 21:53:50 +0800

    feat: parsing post content

--- src/components/blocks/templates/core/post/content.tsx
+++ src/components/blocks/templates/core/post/content.tsx
@@ -1,9 +1,20 @@
 import { BlockComponentProps } from '@src/components/blocks';
+import { usePostContext } from '@src/context/post-context';
+import { Content } from '@src/components/blocks/content';
+import { BlockAttributes } from '@src/lib/block/types';
+import { cn } from '@src/lib/helpers/helper';
 
 export const PostContent = ({ block }: BlockComponentProps) => {
-  if ('core/post-content' !== block.blockName) {
+  const { post } = usePostContext();
+  if ('core/post-content' !== block.blockName || !post?.rawContent) {
     return null;
   }
+  const attribute = block.attrs as BlockAttributes;
+  const { className } = attribute || {};
 
-  return <div>post content</div>;
+  return (
+    <div className={cn('post-content', className)}>
+      <Content content={post.rawContent} />
+    </div>
+  );
 };
