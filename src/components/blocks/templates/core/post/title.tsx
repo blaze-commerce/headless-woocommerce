d0a0934fa5d898d509b5646cfd327c5619dc5cff
jv-lopez <93750956+jv-lopez@users.noreply.github.com>
2024-09-23 11:48:29 +0800

    feat: parsing post title

--- src/components/blocks/templates/core/post/title.tsx
+++ src/components/blocks/templates/core/post/title.tsx
@@ -1,9 +1,15 @@
 import { BlockComponentProps } from '@src/components/blocks';
+import { usePostContext } from '@src/context/post-context';
+import { BlockAttributes } from '@src/lib/block/types';
 
 export const PostTitle = ({ block }: BlockComponentProps) => {
-  if ('core/post-title' !== block.blockName) {
+  const { post } = usePostContext();
+  if ('core/post-title' !== block.blockName || !post) {
     return null;
   }
 
-  return <>POST TITLE</>;
+  const attribute = block.attrs as BlockAttributes;
+  const { className } = attribute || {};
+
+  return <h1 className={className}>{post.name} </h1>;
 };
