f2f217a0569a3f50e39cd1d05df0198d8a03cc8a
Ridwan Arifandi <orangerdigiart@gmail.com>
2024-10-09 01:46:29 +0700

    remove unneeded file

--- src/features/product/product-block.tsx
+++
@@ -1,7 +0,0 @@
-import PRODUCT_DATA from '@public/product.json';
-
-import { Content } from '@src/components/blocks/content';
-
-export const ProductBlock = () => {
-  return <Content content={PRODUCT_DATA} />;
-};
