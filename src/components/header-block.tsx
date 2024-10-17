9cca05702365f42b6e4d7e22a8db0a02537d0698
jv-lopez <93750956+jv-lopez@users.noreply.github.com>
2024-10-17 20:10:29 +0800

    refactor: remove unnecessary code in the header

--- src/components/header-block.tsx
+++ src/components/header-block.tsx
@@ -1,20 +1,11 @@
 import HEADER_DATA from '@public/header.json';
 import { Content } from '@src/components/blocks/content';
 import { useSiteContext } from '@src/context/site-context';
-import { FiX } from 'react-icons/fi';
-import { Search as SearchInput } from '@src/components/header/search';
-import { Search as SearchProps } from '@src/models/settings/search';
 import { WishList } from '@src/features/wish-list';
 import { cn } from '@src/lib/helpers/helper';
 
 export const Header = () => {
-  const { showSearch, setShowSearch, settings } = useSiteContext();
-  const { input, results } = settings?.search as SearchProps;
-  const searchAttributes = {
-    input,
-    results,
-  };
-
+  const { settings } = useSiteContext();
   return (
     <>
       <div id="top"></div>
@@ -25,19 +16,6 @@ export const Header = () => {
       >
         <Content content={HEADER_DATA} />
       </header>
-      {showSearch && (
-        <div className="search absolute w-full top-0 bg-white z-10">
-          <div className="p-4 pb-0 border-b">
-            <FiX
-              className="text-brand-font ml-auto h-6 w-6 mb-4"
-              onClick={() => setShowSearch(false)}
-            />
-          </div>
-          <div className="p-4 bg-white shadow">
-            <SearchInput {...searchAttributes} />
-          </div>
-        </div>
-      )}
       <WishList />
     </>
   );
