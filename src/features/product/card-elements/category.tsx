71ae476a551dfea9c25549790bfb1b35cd06d843
jv-lopez <93750956+jv-lopez@users.noreply.github.com>
2024-12-09 14:30:42 +0800

    fix: ampersand not showing properly in the frontend

--- src/features/product/card-elements/category.tsx
+++ src/features/product/card-elements/category.tsx
@@ -1,3 +1,4 @@
+import { htmlParser } from '@src/lib/block/react-html-parser';
 import { Product } from '@src/models/product';
 
 type ICardProductCategory = {
@@ -9,7 +10,7 @@ export const CardProductCategory: React.FC<ICardProductCategory> = (props) => {
 
   if (!product || !product.categories || product.categories.length === 0) return null;
 
-  const categories = product.categories.map((category) => category.name);
+  const categories = product.categories.map((category) => htmlParser(category.name));
 
   return <div className={'product-category'}>{categories.join(', ')}</div>;
 };
